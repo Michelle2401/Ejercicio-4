ee5127cfc83fd92cb3880141fe51098b4831edfe
Michelle2401 <116472057+Michelle2401@users.noreply.github.com>
2022-10-29 00:12:30 -0600

    DatosUs
    Datos de usuarios

---
+++ Ejercicio-4/Datos/ConexionCadena.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public static class ConexionCadena
+    {
+        public static string Cadena = "Data Source=localhost; Initial catalog=MiLogin; User ID=root; Password=teamoDios2401";
+    }
+}
